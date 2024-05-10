9c79200bf7b7b2a504449ff65f66548fa4bf6ba1
delicloredana <loredana.delic@agilo.co>
2024-05-10 12:10:37 +0200

    creating returns in odoo

--- odoo/src/services/odoo.ts
+++ odoo/src/services/odoo.ts
@@ -1,8 +1,10 @@
-import { TransactionBaseService } from "@medusajs/medusa";
+import {
+  TransactionBaseService,
+} from "@medusajs/medusa";
 import Odoo from "odoo-await";
-
+type OdooType = Odoo & { uid?: number };
 class OdooService extends TransactionBaseService {
-  protected odooClient;
+  protected odooClient: OdooType;
 
   constructor(container) {
     super(container);
@@ -13,26 +15,81 @@ class OdooService extends TransactionBaseService {
       password: process.env.ODOO_PASSWORD,
     });
   }
-  async createMoves(productId: number, quantity: number) {
-    return await this.odooClient.create("stock.move", {
-      product_id: productId,
-      location_id: 8,
-      location_dest_id: 5,
-      name: productId,
-      quantity: quantity,
-      product_uom_qty: quantity,
-    });
-  }
-  async createDeliveryOrder(moves: number[]) {
+
+  async createMoves(
+    productId: number,
+    quantity: number,
+    locationId: number,
+    locationDestId: number,
+    partnerId?: number
+  ) {
     if (!this.odooClient.uid) {
       await this.odooClient.connect();
     }
-    await this.odooClient.create("stock.picking", {
-      location_id: 8,
-      location_dest_id: 5,
-      picking_type_id: 2,
-      move_ids: moves,
+    return await this.odooClient.create("stock.move", {
+      product_id: productId,
+      location_id: locationId,
+      location_dest_id: locationDestId,
+      name: productId,
+      quantity: quantity,
+      product_uom_qty: quantity,
+      partner_id: partnerId,
     });
+  }
+  async createOrder(
+    moves: number[],
+    locationId: number,
+    locationDestId: number,
+    pickingTypeId: number,
+    returnId?,
+    origin?
+  ) {
+    if (!this.odooClient.uid) {
+      await this.odooClient.connect();
+    }
+    return await this.odooClient.create("stock.picking", {
+      location_id: locationId,
+      location_dest_id: locationDestId,
+      picking_type_id: pickingTypeId,
+      move_ids: moves,
+      return_id: returnId,
+      origin,
+    });
+  }
+  async createReturnOrder(moves, locationId, pickingId) {
+    if (!this.odooClient.uid) {
+      await this.odooClient.connect();
+    }
+    return await this.odooClient.create("stock.return.picking", {
+      location_id: locationId,
+      picking_id: pickingId,
+      product_return_moves: moves,
+    });
+  }
+  async createReturnMoves(productId, quantity, moveIds) {
+    if (!this.odooClient.uid) {
+      await this.odooClient.connect();
+    }
+    const move = await this.createMoves(productId, quantity, 5, 8);
+    const returnMoves = await this.odooClient.create(
+      "stock.return.picking.line",
+      {
+        product_id: productId,
+        quantity: quantity,
+        move_id: move,
+      }
+    );
+    return {
+      move,
+      returnMoves,
+    };
+  }
+
+  async getDeliveryOrder(pickingId) {
+    if (!this.odooClient.uid) {
+      await this.odooClient.connect();
+    }
+    return await this.odooClient.read("stock.picking", [pickingId]);
   }
   async getProduct(externalId: string) {
     if (!this.odooClient.uid) {
--- odoo/src/subscribers/odoo.ts
+++ odoo/src/subscribers/odoo.ts
@@ -2,30 +2,86 @@ import {
   type SubscriberConfig,
   type SubscriberArgs,
   OrderService,
+  ReturnService,
 } from "@medusajs/medusa";
 import OdooService from "src/services/odoo";
-export default async function odoInventoryHandler({
+export default async function odooInventoryHandler({
   data,
   container,
 }: SubscriberArgs<Record<string, any>>) {
   const orderService: OrderService = container.resolve("orderService");
+  const returnService: ReturnService = container.resolve("returnService");
   const odooService: OdooService = container.resolve("odooService");
-
-  const { id } = data;
-
-  const order = await orderService.retrieve(id, { relations: ["items"] });
   const moves = [];
-  for (const item of order.items) {
-    const product = await odooService.getProduct(item.variant_id);
-    const move = await odooService.createMoves(product.id, item.quantity);
-    moves.push(move);
+  const { id, return_id } = data;
+
+  if (return_id) {
+    const returnData = await returnService.retrieve(return_id, {
+      relations: [
+        "order",
+        "items",
+        "order",
+        "items.item",
+        "shipping_method",
+        "shipping_method.shipping_option",
+      ],
+    });
+    const order = await orderService.retrieve(id, { select: ["metadata"] });
+    const delivery = (await odooService.getDeliveryOrder(
+      order.metadata.picking_id
+    )) as any[];
+    const returnMoves = [];
+    for (const item of returnData.items) {
+      const product = (await odooService.getProduct(
+        item.item.variant_id
+      )) as any;
+      const result = await odooService.createReturnMoves(
+        product.id,
+        item.quantity,
+        delivery[0].move_ids
+      );
+      moves.push(result.move);
+      returnMoves.push(result.returnMoves);
+    }
+    await odooService.createOrder(
+      moves,
+      5,
+      8,
+      1,
+      order.metadata?.picking_id,
+      `Return of ${delivery[0].name}`
+    );
+  } else {
+    const order = await orderService.retrieve(id, {
+      relations: [
+        "items",
+        "customer",
+        "shipping_address",
+        "shipping_methods",
+        "shipping_methods.shipping_option",
+      ],
+    });
+
+    for (const item of order.items) {
+      const product = (await odooService.getProduct(item.variant_id)) as any;
+      const move = await odooService.createMoves(
+        product.id,
+        item.quantity,
+        8,
+        5
+      );
+      moves.push(move);
+    }
+    const pickingId = await odooService.createOrder(moves, 8, 5, 2);
+    await orderService.update(order.id, {
+      metadata: { picking_id: pickingId },
+    });
   }
-  await odooService.createDeliveryOrder(moves);
 }
 
 export const config: SubscriberConfig = {
-  event: OrderService.Events.PLACED,
+  event: [OrderService.Events.PLACED, OrderService.Events.RETURN_REQUESTED],
   context: {
-    subscriberId: "odo-inventory-handler",
+    subscriberId: "odoo-inventory-handler",
   },
 };
