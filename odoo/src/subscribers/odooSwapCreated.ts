6e09d5c914b88d89798ad4e0a9d8354546b5c193
delicloredana <loredana.delic@agilo.co>
2024-05-15 13:07:43 +0200

    creating swap

---
+++ odoo/src/subscribers/odooSwapCreated.ts
@@ -0,0 +1,95 @@
+import {
+  type SubscriberConfig,
+  type SubscriberArgs,
+  OrderService,
+  SwapService,
+} from "@medusajs/medusa";
+import OdooService from "src/services/odoo";
+export default async function odooSwapCreatedHandler({
+  data,
+  container,
+}: SubscriberArgs<Record<string, any>>) {
+  const orderService: OrderService = container.resolve("orderService");
+  const swapService: SwapService = container.resolve("swapService");
+  const odooService: OdooService = container.resolve("odooService");
+
+  const { id } = data;
+
+  const swap = await swapService.retrieve(id, {
+    select: [
+      "order.display_id",
+      "order.currency_code",
+      "id",
+      "difference_due",
+      "cart.total",
+      "order_id",
+    ],
+    relations: [
+      "additional_items",
+      "order",
+      "order.shipping_address",
+      "return_order",
+      "return_order.items",
+      "return_order.items.item",
+      "return_order.shipping_method",
+      "return_order.shipping_method.shipping_option",
+    ],
+  });
+  const order = await orderService.retrieve(swap.order_id);
+  const partnerId = await odooService.findPartner(
+    swap.order.email,
+    swap.order.shipping_address.address_1,
+    swap.order.shipping_address.city,
+    swap.order.shipping_address.country_code,
+    swap.order.shipping_address.first_name +
+      " " +
+      swap.order.shipping_address.last_name
+  );
+  const delivery = (await odooService.getDeliveryOrder(
+    order.metadata.picking_id
+  )) as any[];
+  const returnMoves = [];
+  for (const item of swap.return_order.items) {
+    const product = (await odooService.getProduct(item.item.variant_id)) as any;
+    const result = await odooService.createReturnMoves(
+      product.id,
+      item.quantity,
+      delivery[0].move_ids
+    );
+    returnMoves.push(result.move);
+  }
+  await odooService.createOrder(
+    returnMoves,
+    5,
+    8,
+    1,
+    partnerId,
+    `Swap for ${delivery[0].name}`,
+    order.metadata?.picking_id as number
+  );
+
+  const additionalMoves = [];
+  for (const item of swap.additional_items) {
+    const product = (await odooService.getProduct(item.variant_id)) as any;
+    const move = await odooService.createMoves(product.id, item.quantity, 8, 5);
+    additionalMoves.push(move);
+  }
+  const pickingId = await odooService.createOrder(
+    additionalMoves,
+    8,
+    5,
+    2,
+    partnerId,
+    `Swap for ${delivery[0].name}`
+  );
+  await swapService.update(id, {
+    metadata: { picking_id: pickingId },
+  });
+}
+
+export const config: SubscriberConfig = {
+  event: [SwapService.Events.CREATED],
+  context: {
+    subscriberId: "odoo-swap-created-handler",
+  },
+};
