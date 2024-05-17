29109ba67bf488863146317cc160befd194936e3
delicloredana <loredana.delic@agilo.co>
2024-05-17 14:26:42 +0200

    using env

--- odoo/src/commands/odooProductQuantity.ts
+++ odoo/src/commands/odooProductQuantity.ts
@@ -1,13 +1,9 @@
-import {
-  ProductService,
-} from "@medusajs/medusa";
+import { ProductService } from "@medusajs/medusa";
 import express from "express";
 import loaders from "@medusajs/medusa/dist/loaders";
 import odoo from "odoo-await";
 import path from "path";
-const odooProductQuantity = async (
-
-): Promise<void> => {
+const odooProductQuantity = async (): Promise<void> => {
   console.info("Starting loader...");
   const app = express();
   const directory = path.resolve(__dirname, "../../");
@@ -19,10 +15,10 @@ const odooProductQuantity = async (
   const productService = container.resolve<ProductService>("productService");
   const products = await productService.list({}, { relations: ["variants"] });
   const odooClient = new odoo({
-    baseUrl: "https://medusa9.odoo.com",
-    db: "medusa9",
-    username: "loredanadelic@gmail.com",
-    password: "e01da446ebe16f852a310eebb33368088a14c30a",
+    baseUrl: process.env.ODOO_BASE_URL,
+    db: process.env.ODOO_DB,
+    username: process.env.ODOO_USERNAME,
+    password: process.env.ODOO_PASSWORD,
   });
   await odooClient.connect();
   for (const product of products) {
